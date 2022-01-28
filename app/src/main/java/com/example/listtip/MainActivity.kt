fdd661d61503788f252fb43752b95ccff51f925c
Darkknight123 <philipkerry57@gmail.com>
2022-01-28 09:45:53 +0300

    initial commit

---
+++ app/src/main/java/com/example/listtip/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.listtip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
