e98b7e1437180912ef8a9950181164a909459cdd
Darkknight123 <philipkerry57@gmail.com>
2022-02-04 22:38:11 +0300

    calculateTip logic added

--- app/src/main/java/com/example/listtip/MainActivity.kt
+++ app/src/main/java/com/example/listtip/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.listtip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import com.example.listtip.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculate.setOnClickListener{calculateTip()}
+    }
+
+    fun calculateTip(){
+        val stringInTextField =  binding.cost.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectId =binding.radioGroup.checkedRadioButtonId
+
+        val tipPercentage = when (selectId){
+            R.id.radio1 -> 0.20
+            R.id.radio2 -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.rounupSwitch.isChecked
+        if (roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
     }
 }
+
